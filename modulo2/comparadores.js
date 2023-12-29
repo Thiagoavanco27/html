3a06f0b4aef51f0fd5cd4f8fc8cc6d42a53e3567
Thiago <thiago.reis@aluno.faculdadeimpacta.com.br>
2023-12-28 22:06:01 -0300

    comparadores

---
+++ modulo2/comparadores.js
@@ -0,0 +1,23 @@
+let x = 2
+
+console.log(x > 1);
+
+console.log(x > 10);
+
+console.log(x > 10);
+
+console.log(x > 2);
+
+console.log(x >= 2);
+
+console.log(x <= 2);
+
+console.log( x === 2);
+
+console.log(x == "2");
+
+console.log(x == "dois");
+
+console.log(x !== 2);
+
+console.log(x !== 1)
